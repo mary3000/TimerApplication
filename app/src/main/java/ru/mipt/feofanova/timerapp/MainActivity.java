7c65be8a984fbea92041e300dd5e52044f849896
mary <mary-fifa3000@mail.ru>
2017-10-09 14:02:36 +0300

    fix after code review

--- app/src/main/java/ru/mipt/feofanova/timerapp/MainActivity.java
+++ app/src/main/java/ru/mipt/feofanova/timerapp/MainActivity.java
@@ -7,14 +7,34 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    static final String KEY_TIMER_VALUE_ = "TIMER";
+    long MILLI_SECONDS = 2000;
+
+    static CountDownTimer timer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
 
-        new CountDownTimer(2000, 1000) {
+    @Override
+    public void onPause() {
+        super.onPause();
 
-            public void onTick(long sec) {}
+        timer.cancel();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        timer = new CountDownTimer(MILLI_SECONDS, 10) {
+
+            public void onTick(long milliSeconds) {
+                MILLI_SECONDS = milliSeconds;
+            }
 
             public void onFinish() {
                 Intent intent = new Intent(MainActivity.this, TimerActivity.class);
@@ -23,4 +43,17 @@ public class MainActivity extends AppCompatActivity {
             }
         }.start();
     }
+
+    @Override
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
+        MILLI_SECONDS = savedInstanceState.getLong(KEY_TIMER_VALUE_);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        outState.putLong(KEY_TIMER_VALUE_, MILLI_SECONDS);
+    }
+
 }
--- app/src/main/java/ru/mipt/feofanova/timerapp/TimerActivity.java
+++ app/src/main/java/ru/mipt/feofanova/timerapp/TimerActivity.java
@@ -12,7 +12,7 @@ import static java.lang.Math.pow;
 
 public class TimerActivity extends AppCompatActivity {
 
-    private static final SparseArray<String> writtenNumbers =  new SparseArray<String>() {
+    private static final SparseArray<String> WRITTEN_NUMBERS =  new SparseArray<String>() {
         {
             append(1, "один");
             append(2, "два");
@@ -61,102 +61,103 @@ public class TimerActivity extends AppCompatActivity {
     private static final String KEY_BUTTON_ = "BUTTON";
     private static final String KEY_TEXT_ = "TEXT";
 
-    final int[] seconds = {0};
+    int seconds = 0;
 
-    static final String BUTTON_START = "Start";
-    static final String BUTTON_STOP = "Stop";
+    enum State {
+        START, STOP
+    }
+    State buttonState = State.START;
 
     static final int TIMER_VALUE = 1001;
     static final int TIMER_INTERVAL = 1000;
 
-    static CountDownTimer timer;
+    CountDownTimer timer;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timer);
 
+        final String buttonStart = getString(R.string.start);
+        final String buttonStop = getString(R.string.stop);
+
         final Button button = (Button)findViewById(R.id.button);
 
-        if (savedInstanceState != null) {
-            restoreValues(savedInstanceState, button);
-        } else {
-            button.setText(BUTTON_START);
-        }
+        button.setText(buttonStart);
+        buttonState = State.START;
 
-        long allTime = (TIMER_VALUE - seconds[0])*1000;
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (buttonState == State.START) {
+                    button.setText(buttonStop);
+                    buttonState = State.STOP;
+                    timer.start();
+
+                } else if (buttonState == State.STOP) {
+                    button.setText(buttonStart);
+                    buttonState = State.START;
+                    timer.cancel();
+                }
+            }
+
+        });
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        final String buttonStart = getString(R.string.start);
+
+        final TextView textView = ((TextView)findViewById(R.id.text));
+        final Button button = (Button)findViewById(R.id.button);
+
+        long allTime = (TIMER_VALUE - seconds)*1000;
         timer = new CountDownTimer(allTime, TIMER_INTERVAL) {
 
-            StringBuffer buff = new StringBuffer();
+            StringBuilder buff = new StringBuilder();
             @Override
             public void onTick(long milliSec) {
-                seconds[0]++;
-                if (seconds[0] >= TIMER_VALUE) {
+                seconds++;
+                if (seconds > TIMER_VALUE) {
                     this.onFinish();
                     this.cancel();
                 }
-                buff.append(writtenNumbers.get(seconds[0], ""));
 
+                buff.append(WRITTEN_NUMBERS.get(seconds, ""));
                 if (buff.length() == 0) {
                     parseNumber(buff);
                 }
 
-                ((TextView)findViewById(R.id.text)).setText(buff.toString());
+                textView.setText(buff.toString());
                 buff.delete(0, buff.length());
             }
 
             @Override
             public void onFinish() {
-                button.setText(BUTTON_START);
+                button.setText(buttonStart);
+                buttonState = State.START;
+
                 buff.delete(0, buff.length());
-                seconds[0] = 0;
+                seconds = 0;
             }
         };
 
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String buttonString = button.getText().toString();
-
-                if (buttonString.equals(BUTTON_START)) {
-                    button.setText(BUTTON_STOP);
-                    timer.start();
-                } else if (buttonString.equals(BUTTON_STOP)) {
-                    button.setText(BUTTON_START);
-                    timer.cancel();
-                }
-
-            }
-        });
-
-        if (savedInstanceState != null) {
-            if (button.getText() == BUTTON_STOP) {
-                timer.start();
-            } else {
-                String textString = savedInstanceState.getString(KEY_TEXT_, "");
-                ((TextView)findViewById(R.id.text)).setText(textString);
-            }
-        }
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-
-        timer.cancel();
-    }
-
-    @Override
-    public void onRestart() {
-        super.onRestart();
-
-        if (((Button)findViewById(R.id.button)).getText() == BUTTON_STOP) {
+        if (buttonState == State.STOP) {
             timer.start();
         }
     }
 
-    public void parseNumber(StringBuffer buff) {
-        int currentNumber = seconds[0];
+    @Override
+    public void onPause() {
+        super.onPause();
+
+        timer.cancel();
+    }
+
+    public void parseNumber(StringBuilder buff) {
+        int currentNumber = seconds;
         for (int i = 0; currentNumber > 0; i++) {
             int numeral = currentNumber % 10;
             currentNumber = currentNumber / 10;
@@ -166,32 +167,35 @@ public class TimerActivity extends AppCompatActivity {
             }
 
             if (i == 0 && currentNumber % 10 == 1) {
-                buff.insert(0, writtenNumbers.get(numeral + 10) + " ");
+                buff.insert(0, WRITTEN_NUMBERS.get(numeral + 10) + " ");
                 currentNumber = currentNumber / 10;
                 i++;
             } else {
-                buff.insert(0, writtenNumbers.get(numeral*(int)pow(10, i)) + " ");
+                buff.insert(0, WRITTEN_NUMBERS.get(numeral*(int)pow(10, i)) + " ");
             }
         }
-    }
-
-    public void restoreValues(Bundle saved, Button button) {
-        seconds[0] = saved.getInt(KEY_SECONDS_, -1);
-
-        String textString = saved.getString(KEY_TEXT_, "def");
-        String buttonString = saved.getString(KEY_BUTTON_, "def");
-
-        ((TextView)findViewById(R.id.text)).setText(textString);
-        button.setText(buttonString);
     }
 
     @Override
     public void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
 
-        outState.putInt(KEY_SECONDS_, seconds[0]);
+        outState.putInt(KEY_SECONDS_, seconds);
         outState.putString(KEY_BUTTON_, ((Button)(findViewById(R.id.button))).getText().toString());
         outState.putString(KEY_TEXT_, ((TextView)(findViewById(R.id.text))).getText().toString());
     }
-}
 
+    @Override
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
+        final String BUTTON_START = getString(R.string.start);
+
+        seconds = savedInstanceState.getInt(KEY_SECONDS_);
+
+        String textString = savedInstanceState.getString(KEY_TEXT_, "");
+        String buttonString = savedInstanceState.getString(KEY_BUTTON_);
+        buttonState = buttonString.equals(BUTTON_START) ? State.START : State.STOP;
+
+        ((TextView)findViewById(R.id.text)).setText(textString);
+        ((Button)findViewById(R.id.button)).setText(buttonString);
+    }
+}
